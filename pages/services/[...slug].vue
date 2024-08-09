a6169c8b69d4a4db7be6a75354f0799687313281
PINF54 <APOINTET-ext@bm-energies.com>
2024-08-09 22:22:39 +0200

    Create architecture detail

--- pages/services/[...slug].vue
+++ pages/services/[...slug].vue
@@ -1,3 +1,26 @@
 <template>
-    En cours de conception
+    <container>
+        <ContentDoc v-slot="{ doc }">
+            <article class="service-detail">
+                <h1>{{ doc.title }}</h1>
+                <ContentRenderer :value="doc" />
+            </article>
+        </ContentDoc>
+    </container>
+    <NuxtPicture format="webp" src="/img/logo-800.png" alt="Forelse" class="header--logo mr-8 col-span-1 w-full"
+               :img-attrs="{ class: 'h-[350px] mx-auto'}"/>
 </template>
+
+<style>
+.service-detail h1 {
+    @apply mt-12 mb-8 font-title text-primary text-2xl;
+}
+
+.service-detail {
+    @apply max-w-[800px] mx-auto leading-relaxed mb-12;
+}
+
+.service-detail p {
+    @apply mb-4;
+}
+</style>
