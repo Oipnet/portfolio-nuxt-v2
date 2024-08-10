5bf98f195d0ea8872e2bfe6162ed01dd611aa95e
PINF54 <APOINTET-ext@bm-energies.com>
2024-08-10 17:29:26 +0200

    Add cover to services

--- pages/services/[...slug].vue
+++ pages/services/[...slug].vue
@@ -3,6 +3,8 @@
         <ContentDoc v-slot="{ doc }">
             <article class="service-detail">
                 <h1>{{ doc.title }}</h1>
+                <NuxtPicture v-if="doc.cover" format="webp" :src="doc.cover" alt="Forelse" class="float-right h-[350px] mb-2"
+                    :img-attrs="{ class: 'object-cover h-[350px] ml-8 rounded-xl'}"/>
                 <ContentRenderer :value="doc" />
             </article>
         </ContentDoc>
@@ -11,6 +13,11 @@
     <NuxtPicture format="webp" src="/img/logo-800.png" alt="Forelse" class="header--logo mr-8 col-span-1 w-full"
                :img-attrs="{ class: 'h-[350px] mx-auto mt-8'}"/>
 </template>
+<script setup lang="ts">
+const fullCoverPath = computed((doc) => {
+    return doc.cover
+})
+</script>
 
 <style>
 .service-detail h1 {
